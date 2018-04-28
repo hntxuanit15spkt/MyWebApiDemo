560e17bf2dcd5f4f9dc6aa44d1f7debfde1a9ec5
hntxuanit15spkt <hntxuanit15spkt@gmail.com>
2018-04-28 07:17:46 +0700

    Add project files.

---
+++ HelloWebAPI/Controllers/HelloController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace HelloWebAPI.Controllers
+{
+    public class HelloController : ApiController
+    {
+        //Khi gõ GET api/Hello thì nó sẽ gọi đến phương thức Get trong controller này
+        public string Get()
+        {
+            return "Hello World";
+        }
+    }
+}
---
+++ HelloWebAPI/Global.asax.cs
@@ -0,0 +1,52 @@
+﻿using HelloWebAPI.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace HelloWebAPI
+{
+    //Web API is configured only using code based configuration using GlobalConfiguration class. 
+    public class Global : System.Web.HttpApplication
+    {
+        //when application starts it will call Application_Start event
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            //The Configure() method requires a callback method where you have configured your Web API.
+            GlobalConfiguration.Configure(HelloWebAPIConfig.Register); 
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
